10fe242eaaca166c482bd432d9495097ce918735
joemohd <joemohd.m@gmail.com>
2025-11-12 14:52:13 +0200

    Refactor Mammal.h header file and class definition

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,27 @@
-#ifndef MAMMAL_H
-#define MAMMAL_H
+# ifndef MAMMAL_H
+# define MAMMAL_H
 
-#include "Animal.h"
+# include "Animal.h"
 
-//define mammal class here
+# include <iostream>
+using namespace std;
 
+class Mammal : public Animal {
+    private:
+        string furColour;
 
+    public:
+        Mammal () : Animal () {
+            furColour = "";
+        }
 
+        Mammal (string name, int age, bool isHungry, string furColour)  : Animal (name, age, isHungry) {
+            this->furColour = furColour;
+        }
 
+        ~Mammal () {
+            // does nothing
+        }
+};
 
-
-#endif
+# endif
