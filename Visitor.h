4d5a3a8fac1117d71bd14d7763d80037c7ce28d7
joemohd <joemohd.m@gmail.com>
2025-11-12 14:53:08 +0200

    Refactor Visitor class and update header guards

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,32 @@
-#ifndef VISITOR_H
-#define VISITOR_H
+# ifndef VISITOR_H
+# define VISITOR_H
 
-#include <string>
+# include <iostream>
 using namespace std;
 
-//define visitor class here
+class Visitor {
+    private:
+        string visitorName;
+        int ticketsBought;
 
+    public:
+        Visitor () {
+            visitorName = "";
+            ticketsBought = 0;
+        }
 
+        Visitor (string visitorName, int ticketsBought) {
+            this->visitorName = visitorName;
+            this->ticketsBought = ticketsBought;
+        }
 
+        ~Visitor () {
+            // does nothing
+        }
 
+        void displayInfo () {
+            cout << "Name: " << visitorName << endl << "Tickets Bought: " << ticketsBought << endl;
+        }
+};
 
-
-
-#endif
+# endif
