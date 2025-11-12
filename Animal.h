eff78090e2bb93c053d2e241436686901381d18b
joemohd <joemohd.m@gmail.com>
2025-11-12 14:50:25 +0200

    Refactor Animal class with constructors and methods

--- Animal.h
+++ Animal.h
@@ -1,11 +1,59 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
+# ifndef ANIMAL_H
+# define ANIMAL_H
 
-#include <string>
+# include <iostream>
 using namespace std;
+class Animal {
+    private:
+        string name;
+        int age;
+        bool isHungry;
 
-//define animal class here 
+    public:
+        Animal () {
+            name = "";
+            age = 0;
+            isHungry = false;
+        }
 
+        Animal (string name, int age, bool isHungry) {
+            this->name = name;
+            this->age = age;
+            this->isHungry = isHungry;
+        }
 
+        ~Animal () {
+            // does nothing
+        }
 
-#endif
+        void display () {
+            cout << name << "(Age: " << age << ", ";
+            if (isHungry) {
+                cout << "Hungry";
+            } else {
+                cout << "Not Hungry";
+            }
+            cout << ")" << endl;
+        }
+
+        void feed () {
+            // TODO Make sure that it works as expected
+            if (!isHungry) {
+                isHungry = true;
+            }
+        }
+
+        string getName () {
+            return name;
+        }
+
+        int getAge () {
+            return age;
+        }
+
+        bool getIsHungry () {
+            return isHungry;
+        }
+};
+
+# endif
