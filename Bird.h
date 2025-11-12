6e8569cbe272cf5f57b1b0c245d880a48334507f
joemohd <joemohd.m@gmail.com>
2025-11-12 14:51:15 +0200

    Refactor Bird class with constructors and destructor
    Added constructor and destructor for Bird class.

--- Bird.h
+++ Bird.h
@@ -1,14 +1,27 @@
-#ifndef BIRD_H
-#define BIRD_H
+# ifndef BIRD_H
+# define BIRD_H
 
-#include "Animal.h"
+# include "Animal.h"
 
-//define class bird here 
+# include <iostream>
+using namespace std;
 
+class Bird : public Animal {
+    private:
+        float wingSpan;
 
+    public:
+        Bird () : Animal () {
+            wingSpan = 0.0;
+        }
 
+        Bird (string name, int age, bool isHungry, float wingSpan)  : Animal (name, age, isHungry) {
+            this->wingSpan = wingSpan;
+        }
 
+        ~Bird () {
+            // does nothing
+        }
+};
 
-
-
-#endif
+# endif
