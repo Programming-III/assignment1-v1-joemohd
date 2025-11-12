ab99a6e174aa291c364303bfd9ea0d693b309178
joemohd <joemohd.m@gmail.com>
2025-11-12 14:52:45 +0200

    Refactor Reptile class and include guards

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,43 @@
-#ifndef REPTILE_H
-#define REPTILE_H
+# ifndef REPTILE_H
+# define REPTILE_H
 
-#include "Animal.h"
+# include "Animal.h"
 
-//define reptile class here
+# include <iostream>
+using namespace std;
 
+class Reptile : public Animal {
+    private:
+        bool isVenomous;
 
+    public:
+        Reptile () : Animal () {
+            isVenomous = false;
+        }
 
+        Reptile (string name, int age, bool isHungry, bool isVenomous)  : Animal (name, age, isHungry) {
+            this->isVenomous = isVenomous;
+        }
 
+        ~Reptile () {
+            // does nothing
+        }
 
+        void display () {
+            cout << getName () << "(Age: " << getAge () << ", ";
+            if (isVenomous) {
+                cout << "Venomous";
+            } else {
+                cout << "Not Venomous";
+            }
 
+            if (getIsHungry ()) {
+                cout << "Hungry";
+            } else {
+                cout << "Not Hungry";
+            }
+            cout << ")" << endl;
+        }
+};
 
-
-
-#endif
+# endif
