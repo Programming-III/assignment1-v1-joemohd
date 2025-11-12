5dfb01998979361e8960c14c92cbe1cca89c9fba
joemohd <joemohd.m@gmail.com>
2025-11-12 14:53:38 +0200

    Refactor main function and include order

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
-#include "Animal.h"
-#include "Bird.h"
-#include "Mammal.h"
-#include "Reptile.h"
-#include "Enclosure.h"
-#include "Visitor.h"
-#include <iostream>
-using namespace std;
+# include "Enclosure.h"
+# include "Mammal.h"
+# include "Bird.h"
+# include "Reptile.h"
+# include "Visitor.h"
 
+int main () {
+    Enclosure* enclosure = new Enclosure (50);
 
-// ============== MAIN FUNCTION ==============
-int main() {
-    
-    
+    Mammal* lion = new Mammal ("Lion", 5, true, "yellow");
+    Bird* bird = new Bird ("Parrot", 2, false, 2.3);
+    Reptile* reptile = new Reptile ("Snake", 3, true, true);
+
+    enclosure->addAnimal (lion);
+    enclosure->addAnimal (bird);
+    enclosure->addAnimal (reptile);
+
+    Visitor* visitor = new Visitor ("Sarah Ali", 3);
+
+    cout << "Enclosure 1 Animals:" << endl;
+    enclosure->displayAnimals ();
+    cout << endl;
+
+    cout << "Visitor Info:";
+    visitor->displayInfo ();
+
     return 0;
 }
